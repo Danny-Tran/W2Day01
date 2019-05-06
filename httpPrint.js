57a6dd88b53c7626fae2ddf6033a670294188e72
Danny-Tran <truong_tran@live.com>
2019-05-06 13:31:30 +0000

    completed

--- httpPrint.js
+++ httpPrint.js
@@ -1,11 +1,29 @@
+var https = require('https');
+
 function getAndPrintHTML () {
 
     var requestOptions = {
-      host: 'sytantris.github.io',
-      path: '/http-examples/step2.html'
-    };
-  
-    /* Add your code here */
+        host: 'sytantris.github.io',
+        path: '/http-examples/step2.html'
+      };
+    // var body = '';
+    https.get(requestOptions, function(response){
+        // if(response.statusCode !== 200)
+        var body = '';
+        
+        response.setEncoding('utf8');
+        response.on('data', function(chunk){
+            body += chunk;
+            // console.log('\n ' + body +  ' \n')
+        });
+
+        response.on('end', function(){
+            console.log('\n ' + body +  ' \n')
+            console.log("Response completed")
+        });
+    }); 
+    
   
   }
-  
+
+  getAndPrintHTML ()
